bc6db2968f9fecf4f9c278ae3332cb7dfde6bf2e
demirtasenes4 <demirtasenes2@gmail.com>
2024-03-22 20:38:57 +0300

    structure has been created

--- src/OdevGrup1.Persistence/Context/ApplicationDbContext.cs
+++ src/OdevGrup1.Persistence/Context/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OdevGrup1.Infrastructure.Context;
+namespace OdevGrup1.Persistence.Context;
 internal class ApplicationDbContext
 {
 }
