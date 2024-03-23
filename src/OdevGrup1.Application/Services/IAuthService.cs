4defa6d0cb57a62a10870aa98d3d5fc092361880
Cuma KÖSE <turkmvc@gmail.com>
2024-03-23 17:05:15 +0300

    .

--- src/OdevGrup1.Application/Services/IAuthService.cs
+++
@@ -1,8 +0,0 @@
-﻿using OdevGrup1.Application.Features.Auth.Register;
-using TS.Result;
-
-namespace OdevGrup1.Application.Services;
-public interface IAuthService
-{
-    Task<Result<string>> CreateAsync(RegisterCommand request, CancellationToken cancellationToken); 
-}
